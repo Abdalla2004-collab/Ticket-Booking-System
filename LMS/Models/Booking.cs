6a97efad60f820f2649f3d084668629775a1d6c8
Abdalla2004-collab <abdallabakr8072@gmail.com>
2026-04-08 17:09:58 +0100

    Added base SQL query methods in customers, adding bookings class that hold data for bookings table and modified events to identify total tickets, next would be Forms design and attaching methods to each button/datagrid for viewing

---
+++ LMS/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿namespace LMS.Models;
+
+public class Booking
+{
+    public int bookingId { get; set; }
+    public int customerId { get; set; }
+    public int eventId { get; set; }
+    public int quantity { get; set; }
+    public decimal totalPrice { get; set; }
+    public DateTime bookingDate { get; set; }
+    public string status { get; set; } = "Confirmed";
+    public string   eventTitle  { get; set; } = "";
+    public string   venueName   { get; set; } = "";
+    public DateTime eventDate   { get; set; }
+    public TimeSpan eventTime   { get; set; }
+}
--- LMS/Models/Customer.cs
+++ LMS/Models/Customer.cs
@@ -1,4 +1,5 @@
 ﻿namespace LMS.Models;
+using MySql.Data.MySqlClient;
 
 public class Customer : User
 {
@@ -7,9 +8,163 @@ public class Customer : User
         role = "Customer";
         CreatedAt = DateTime.Now;
     }
-    
+
     public override Form getDashboard()
     {
         return new CustomerDashboard();
+    }
+    
+    public List<Events> getApprovedEvents(string searchTitle = "", string filterCategory = "")
+    {
+        var events = new List<Events>();
+
+        using (MySqlConnection connection = GlobalManager.GetConnection())
+        {
+            connection.Open();
+
+            string query = @"SELECT e.eventId, e.title, e.category, e.eventDate, e.eventTime, e.price, e.status,
+                            v.name as VenueName,
+                            e.totalTickets - COALESCE((SELECT SUM(b.quantity) FROM bookings b Where b.eventId = e.eventId
+                            AND b.status = 'Confirmed'), 0) as availableTickets FROM events e
+                            JOIN venues v on e.venueId = v.venueId 
+                            WHERE e.status = 'Approved' AND e.eventDate>= CURRENT_DATE()
+                            AND (@title = '' OR e.title LIKE @titleSearch)
+                            AND (@category = '' OR e.category LIKE @category)
+                            ORDER BY e.eventDate ASC";
+
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@titleSearch", searchTitle);
+                command.Parameters.AddWithValue("@titleSearch", "%" + searchTitle + "%");
+                command.Parameters.AddWithValue("@category", filterCategory);
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        events.Add(new Events
+                        {
+                            eventId = (int)reader["eventId"],
+                            title = reader["title"].ToString(),
+                            category = reader["category"].ToString(),
+                            eventDate = Convert.ToDateTime(reader["eventDate"]),
+                            eventTime = TimeSpan.Parse(reader["eventTime"].ToString()),
+                            price = Convert.ToDecimal(reader["price"]),
+                            status = reader["status"].ToString(),
+                            venueName = reader["venueName"].ToString(),
+                            availableTickets = Convert.ToInt32(reader["availableTickets"])
+                        });
+                    }
+                }
+            }
+
+            return events;
+        }
+    }
+    
+    public (bool success, string message) bookEvent(int eventId, int quantity, decimal pricePerTicket)
+    {
+        using (MySqlConnection connection = GlobalManager.GetConnection())
+        {
+            connection.Open();
+
+            string checkQuery = @"SELECT e.totalTickets - COALESCE((SELECT SUM (b.quantity) From bookings b
+                                  WHERE b.eventId = @eventId AND b.status = 'Confirmed'),0) AS availableTickets
+                                  from events e where e.eventId = @eventId";
+
+            int available = 0;
+
+            using (MySqlCommand checkCmd = new MySqlCommand(checkQuery, connection))
+            {
+                checkCmd.Parameters.AddWithValue("@eventId", eventId);
+                var result = checkCmd.ExecuteScalar();
+                available = Convert.ToInt32(result);
+            }
+
+            if (available < quantity)
+            {
+                return (false, $"Only {available} tickets are available for this event.");
+            }
+            
+            decimal totalPrice = quantity * pricePerTicket;
+
+            string insertQuery = @"INSERT into bookings (INSERT INTO bookings (customerId, eventId, quantity, totalPrice)
+                                   VALUES (@customerId, @eventId, @quantity, @totalPrice)";
+
+            using (MySqlCommand insertCmd = new MySqlCommand(insertQuery, connection))
+            {
+                insertCmd.Parameters.AddWithValue("@customerId", id);
+                insertCmd.Parameters.AddWithValue("@eventId", eventId);
+                insertCmd.Parameters.AddWithValue("@quantity", quantity);
+                insertCmd.Parameters.AddWithValue("@totalPrice", totalPrice);
+                
+                int rows = insertCmd.ExecuteNonQuery();
+                return rows < 0 ? (true, "Booking confirmed!") : (false, "Booking failed, please try again!!");
+            }
+        }
+    }
+
+    public List<Booking> getMyBookings()
+    {
+        var bookings = new List<Booking>();
+
+        using (MySqlConnection connection = GlobalManager.GetConnection())
+        {  
+            connection.Open();
+
+            string query = @"select b.bookingId, b.eventId, b.quantity, b.totalPrice, b.bookingDate, 
+                                e.title as eventTitle, e.eventDate, e.eventTime, v.name as venueName
+                                from bookings b
+                                JOIN events e on b.eventId = e.eventId
+                                JOIN venues v on e.venueId = v.venueId
+                                where b.customerId = @customerId
+                                order by b.bookingDate DESC";
+
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("customerId", id);
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        bookings.Add(new Booking
+                        {
+                            bookingId = (int)reader["bookingId"],
+                            eventId = (int)reader["eventId"],
+                            quantity = (int)reader["quantity"],
+                            totalPrice = (decimal)reader["totalPrice"],
+                            bookingDate =  Convert.ToDateTime(reader["bookingDate"]),
+                            status = reader["status"].ToString(),
+                            eventTitle = reader["eventTitle"].ToString(),
+                            venueName = reader["venueName"].ToString(),
+                            eventDate = Convert.ToDateTime(reader["eventDate"]),
+                            eventTime = TimeSpan.Parse(reader["eventTime"].ToString()),
+                        });
+                    }
+                }
+            }
+            return bookings;
+        }
+    }
+
+    public bool cancelBooking(int bookingId)
+    {
+        using (MySqlConnection connection = GlobalManager.GetConnection())
+        {
+            connection.Open();
+
+            string query = @"UPDATE bookings SET status = 'Canceled'
+                            WHERE bookingId = @bookingId AND customerId = id";
+
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("bookingId", bookingId);
+                command.Parameters.AddWithValue("customerId", id);
+                
+                int rows = command.ExecuteNonQuery();
+                return rows > 0;
+            }
+        }
     }
 }
